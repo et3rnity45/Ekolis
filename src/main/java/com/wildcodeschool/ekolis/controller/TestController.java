3cbd70e09920a4f5c3408171cb0597230014a399
Quentin Liger <quentinlgr45160@gmail.com>
2019-12-19 15:38:57 +0100

    level done

--- src/main/java/com/wildcodeschool/ekolis/controller/TestController.java
+++ src/main/java/com/wildcodeschool/ekolis/controller/TestController.java
@@ -2,11 +2,15 @@ package com.wildcodeschool.ekolis.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.wildcodeschool.ekolis.entities.Journey;
+import com.wildcodeschool.ekolis.entities.Level;
+import com.wildcodeschool.ekolis.repository.LevelRepository;
 import com.wildcodeschool.ekolis.utils.FilterNode;
 
 @Controller
@@ -14,10 +18,19 @@ public class TestController {
 
 	private FilterNode filterNode = new FilterNode();
 	
-	@GetMapping("/journeys")
-	public String toHome(Model model) {
-		List<Journey> journeys = filterNode.buildJourneys("1.89514;47.89324", "1.90876;47.9115");
-		model.addAttribute("journeys", journeys);
+	@Autowired
+	private LevelRepository levelRepository;
+	
+	@GetMapping("/journey/{id}")
+	public String toHome(Model model, @PathVariable int id) {
+		Level level = levelRepository.findById(id).get();
+		List<Journey> journeys1 = filterNode.buildJourneys(level.getPos1(), level.getPos2());
+		List<Journey> journeys2 = filterNode.buildJourneys(level.getPos2(), level.getPos3());
+		List<Journey> journeys3 = filterNode.buildJourneys(level.getPos3(), level.getPos4());
+		model.addAttribute("level", level);
+		model.addAttribute("journeys1", journeys1);
+		model.addAttribute("journeys2", journeys2);
+		model.addAttribute("journeys3", journeys3);
 		return "home";
 	}
 }
